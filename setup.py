c551aba8e89e6a0913fed0b4513e6a3e252d4d2a
skunkie <skunkie@users.noreply.github.com>
2019-04-30 16:19:41 +0300

    feat: add more package info to setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
+
 from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
 
 
 setup(
@@ -10,8 +17,19 @@ setup(
     author_email='vralib@kovarus.com',
     url='https://github.com/kovarus/vrealize-pysdk',
     keywords=['vralib'],
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    license='Apache License, Version 2.0',
     install_requires=[
         'requests',
     ],
-    classifiers=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
