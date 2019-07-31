skunkie/vrealize-pysdk

commit 2763bb2ec48f3359f05b5e17780545e30d3fbeb2

BREAKING CHANGE: return only the value of `content` from the HTTP Response
When retrieving items from the vRA API we do not need extra data.

vralib/classes.py
@@ -186,18 +186,18 @@ class Session(object):
         """
         Iterates over pages of the HTTP Response.
 
-        :return: python dictionary with the JSON response contents.
+        :return: a list of requested items from the `content` of the response.
         """
 
-        result = self._request(url)
+        result = []
 
-        if result['metadata']['totalPages'] != 1:
-            page = 2
-            while page <= result['metadata']['totalPages']:
-                next_page = self._request(url + '?page=%s' % page)
-                for i in next_page['content']:
-                    result['content'].append(i)
-                page += 1
+        n = 1
+        while True:
+            page = self._request('%s?page=%s' % (url, n))
+            result = result + page['content']
+            if n <= page['metadata']['totalPages']:
+                break
+            n += 1
 
         return result
 
@@ -222,7 +222,7 @@ class Session(object):
 
         result = []
 
-        for i in business_groups['content']:
+        for i in business_groups:
             target = i['name']
             if name.lower() in target.lower():
                 element = {'name': i['name'], 'id': i['id']}
@@ -297,7 +297,7 @@ class Session(object):
 
         result = []
 
-        for i in catalog['content']:
+        for i in catalog:
             target = i['catalogItem']['name']
             if name.lower() in target.lower():
                 element = {'name': i['catalogItem']

get-apiurls.py
@@ -49,7 +49,7 @@ def main():
 
     catalog_item = vra.get_catalogitem_byid(catalog_id)
 
-    catalog_name = (catalog_item["content"][0]["catalogItem"]["name"])
+    catalog_name = (catalog_item[0]["catalogItem"]["name"])
 
     print(f"Catalog Item Name: {catalog_name}")
     print(f"Catalog Item ID: {catalog_id}")

get-items.py
@@ -62,7 +62,7 @@ def main():
     # TODO VMware doesn't appear to use the same format for data it gets back from the API
     # TODO will need to check for this and deal with it somehow.
     # TODO I think I need to determine what the item is first then based on that find the label
-    for i in items['content']:
+    for i in items:
 
         out.add_row((i['name'], i['id']))
         # print i['id']

report-roles.py
@@ -68,7 +68,7 @@ def main():
     with open(outfile, "w") as f:
         csv_file = csv.writer(f)
         csv_file.writerow(['user', 'domain', 'type', 'id', 'role', 'scope'])
-        for i,val in enumerate(subtenantroles['content']):
+        for i, val in enumerate(subtenantroles):
             if val['name'] == 'Basic User':
                 scoperole = val['scopeRoleRef']
                 attype = val['@type']

tool-samples/get-items.py
@@ -62,7 +62,7 @@ def main():
     # TODO VMware doesn't appear to use the same format for data it gets back from the API
     # TODO will need to check for this and deal with it somehow.
     # TODO I think I need to determine what the item is first then based on that find the label
-    for i in items['content']:
+    for i in items:
 
         out.add_row((i['name'], i['id']))
         # print i['id']

commit b476177260bfd5850479950dd71df22c1f518901

fix: a regression in commit 2763bb2e in method _iterate_pages

vralib/classes.py
@@ -195,7 +195,7 @@ class Session(object):
         while True:
             page = self._request('%s?page=%s' % (url, n))
             result += page['content']
-            if n <= page['metadata']['totalPages'] or \
+            if n == page['metadata']['totalPages'] or \
                     page['metadata']['totalElements'] == 0:
                 break
             n += 1